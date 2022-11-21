def2befe39fe24a609f94278dc85441745d68c77
stanken_a <artyemst3@mail.ru>
2022-11-21 18:44:24 +0300

    feat: add popup, finish calculator of price

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,91 @@
-//Функция для соединения значений слайдера и инпута
+// Константы слайдеров
+
+const sliderRam = document.querySelector('#ram');
+const ramResult = document.querySelector('#ram-result');
+
+const sliderCores = document.querySelector('#cores');
+const coresResult = document.querySelector('#cores-result');
+
+const sliderHddSsd = document.querySelector('#hdd-ssd');
+const hddSsdResult = document.querySelector('#hdd-ssd-result');
+
+const sliderNvme = document.querySelector('#nvme');
+const nvmeResult = document.querySelector('#nvme-result');
+
+// Функция расчёта финальной стоимости с учётом чекбоксов
+
+const calcResult = document.querySelector('.calculate__list-item-price');
+
+const sqlCheck = document.querySelector('#sql');
+const msLicenseCheck = document.querySelector('#ms-license');
+
+function calcPrice() {
+
+  let result = (parseInt(sliderRam.value, 10) * 190) + (parseInt(sliderCores.value, 10) * 510) + (parseInt(sliderHddSsd.value, 10) * 4) + (parseInt(sliderNvme.value, 10) * 10);
+
+  if (sqlCheck.checked && msLicenseCheck.checked) {
+    result += 950 + 250;
+  } else if (msLicenseCheck.checked) {
+    result += 950;
+  } else if (sqlCheck.checked) {
+    result += 250;
+  };
+
+  calcResult.textContent = result;
+}
+
+// Обработка событий для чекбоксов
+
+const checkBoxes = document.querySelectorAll('.calculate__checkmark');
+
+checkBoxes.forEach(function () {
+  sqlCheck.addEventListener('change', () => calcPrice());
+  msLicenseCheck.addEventListener('change', () => calcPrice());
+})
+
+//Функция для соединения значений слайдера, инпута и калькулятора
 
 function connectSlider(slider, result) {
 
   slider.oninput = () => {
     result.value = slider.value;
+    calcPrice();
   };
 
   result.oninput = () => {
     slider.value = result.value;
+    calcPrice();
   };
+
 };
 
-// Слайдеры
-
-const sliderRam = document.querySelector('#ram');
-const ramResult = document.querySelector('#ram-result');
+// Вызов функций
 
 connectSlider(sliderRam, ramResult);
-
-const sliderCores = document.querySelector('#cores');
-const coresResult = document.querySelector('#cores-result');
-
 connectSlider(sliderCores, coresResult);
-
-const sliderHddSsd = document.querySelector('#hdd-ssd');
-const hddSsdResult = document.querySelector('#hdd-ssd-result');
-
 connectSlider(sliderHddSsd, hddSsdResult);
-
-const sliderNvme = document.querySelector('#nvme');
-const nvmeResult = document.querySelector('#nvme-result');
-
 connectSlider(sliderNvme, nvmeResult);
 
+//Попап
+
+const popup = document.querySelector('.popup');
+const popupCloseBtn = document.querySelector('.popup__close-button');
+const allPopupButtons = document.querySelectorAll('.button_popup');
+const contactsForm = document.querySelector('.contacts__form');
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+};
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+};
+
+allPopupButtons.forEach(function (item) {
+  item.addEventListener('click', () => openPopup());
+});
+
+popupCloseBtn.addEventListener('click', () => closePopup());
+contactsForm.addEventListener('submit', () => closePopup());
 
 
