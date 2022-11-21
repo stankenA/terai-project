5a0cd6fd176bec1dbae3ac305c9d54027ee6d40e
stanken_a <artyemst3@mail.ru>
2022-11-21 12:01:36 +0300

    feat: add js connection of slider/result

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,37 @@
+//Функция для соединения значений слайдера и инпута
+
+function connectSlider(slider, result) {
+
+  slider.oninput = () => {
+    result.value = slider.value;
+  };
+
+  result.oninput = () => {
+    slider.value = result.value;
+  };
+};
+
 // Слайдеры
 
 const sliderRam = document.querySelector('#ram');
+const ramResult = document.querySelector('#ram-result');
 
-function rangeSlider(value) {
-  document.querySelector('#ram-result').value = value;
-};
+connectSlider(sliderRam, ramResult);
 
-sliderRam.addEventListener('mousemove', () => {
-  rangeSlider(this.value);
-});
+const sliderCores = document.querySelector('#cores');
+const coresResult = document.querySelector('#cores-result');
+
+connectSlider(sliderCores, coresResult);
+
+const sliderHddSsd = document.querySelector('#hdd-ssd');
+const hddSsdResult = document.querySelector('#hdd-ssd-result');
+
+connectSlider(sliderHddSsd, hddSsdResult);
+
+const sliderNvme = document.querySelector('#nvme');
+const nvmeResult = document.querySelector('#nvme-result');
+
+connectSlider(sliderNvme, nvmeResult);
+
+
+
