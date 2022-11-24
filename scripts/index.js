e7a0a4019133fa0a4a6b14f4cc86fb469a557180
stanken_a <artyemst3@mail.ru>
2022-11-24 15:38:36 +0300

    feat: add swiper final version

--- scripts/index.js
+++ scripts/index.js
@@ -88,4 +88,16 @@ allPopupButtons.forEach(function (item) {
 popupCloseBtn.addEventListener('click', () => closePopup());
 contactsForm.addEventListener('submit', () => closePopup());
 
-
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 120,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
